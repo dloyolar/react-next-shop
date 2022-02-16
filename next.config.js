397129958fd564d4fb7b46ce2b27c3372949fb1d
dloyolar <dieegolr@gmail.com>
2022-02-16 17:59:36 -0300

    [ADD] ✨ initial config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    customKey: 'customValue',
+  },
+  basePath: '/dist',
+  compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/hola',
+        destination: 'https://github.com/dloyolar',
+        permanent: true,
+      },
+    ];
+  },
+};
